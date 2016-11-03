d33b22c62cbe3040d2e232ebb07e23f2a663017d
flyingfoxweb <erin@flyingfox.net.nz>
2016-11-04 11:00:09 +1300

    Initial commit.
    Added all files.

---
+++ Project 3 Responsive Contact Form/js/form.js
@@ -0,0 +1,7 @@
+var submitForm = document.getElementById("submitButton");
+
+var submitMessage = function() {
+  alert("Thanks for joining our mail-list!");
+}
+
+submitForm.addEventListener("submit", submitMessage);
